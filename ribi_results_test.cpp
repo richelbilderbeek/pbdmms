cccf4e9368cdeb70a5bf910e550bba97e9f386d5
richelbilderbeek <richel@richelbilderbeek.nl>
2016-10-06 14:45:48 +0200

    Do not convert .dot to SVG to PNG, as this stall the build now

--- ribi_results_test.cpp
+++ ribi_results_test.cpp
@@ -132,10 +132,11 @@ BOOST_AUTO_TEST_CASE(test_results_example_complete_speciation)
     f << r.get_sil_frequency_phylogeny();
     BOOST_TEST_PASSPOINT();
     BOOST_CHECK(is_regular_file(filename_dot));
-    convert_dot_to_svg(filename_dot, filename_svg);
-    BOOST_TEST_PASSPOINT();
-    convert_svg_to_png(filename_svg, filename_png);
-    BOOST_TEST_PASSPOINT();
+    if (!"local computer")
+    {
+      convert_dot_to_svg(filename_dot, filename_svg);
+      convert_svg_to_png(filename_svg, filename_png);
+    }
   }
   r.summarize_sil_frequency_phylogeny();
   {
@@ -150,8 +151,11 @@ BOOST_AUTO_TEST_CASE(test_results_example_complete_speciation)
     f << r.get_summarized_sil_frequency_phylogeny();
     BOOST_TEST_PASSPOINT();
     BOOST_CHECK(is_regular_file(filename_dot));
-    convert_dot_to_svg(filename_dot, filename_svg);
-    convert_svg_to_png(filename_svg, filename_png);
+    if (!"local computer")
+    {
+      convert_dot_to_svg(filename_dot, filename_svg);
+      convert_svg_to_png(filename_svg, filename_png);
+    }
   }
   const auto g = r.get_summarized_sil_frequency_phylogeny();
   /*
@@ -303,8 +307,11 @@ BOOST_AUTO_TEST_CASE(test_results_example_unsuccessfull_speciation)
     std::ofstream f(filename_dot);
     f << r.get_sil_frequency_phylogeny();
     BOOST_CHECK(is_regular_file(filename_dot));
-    convert_dot_to_svg(filename_dot, filename_svg);
-    convert_svg_to_png(filename_svg, filename_png);
+    if (!"local computer")
+    {
+      convert_dot_to_svg(filename_dot, filename_svg);
+      convert_svg_to_png(filename_svg, filename_png);
+    }
     //const std::string cmd{"display " + filename_png};
     //std::system(cmd.c_str());
   }
@@ -321,8 +328,11 @@ BOOST_AUTO_TEST_CASE(test_results_example_unsuccessfull_speciation)
     f << r.get_summarized_sil_frequency_phylogeny();
     BOOST_TEST_PASSPOINT();
     BOOST_CHECK(is_regular_file(filename_dot));
-    convert_dot_to_svg(filename_dot, filename_svg);
-    convert_svg_to_png(filename_svg, filename_png);
+    if (!"local computer")
+    {
+      convert_dot_to_svg(filename_dot, filename_svg);
+      convert_svg_to_png(filename_svg, filename_png);
+    }
     //const std::string cmd{"display " + filename_png};
     //std::system(cmd.c_str());
   }
@@ -471,8 +481,11 @@ BOOST_AUTO_TEST_CASE(test_results_example_problem_case)
     std::ofstream f(filename_dot);
     f << r.get_sil_frequency_phylogeny();
     BOOST_CHECK(is_regular_file(filename_dot));
-    convert_dot_to_svg(filename_dot, filename_svg);
-    convert_svg_to_png(filename_svg, filename_png);
+    if (!"local computer")
+    {
+      convert_dot_to_svg(filename_dot, filename_svg);
+      convert_svg_to_png(filename_svg, filename_png);
+    }
   }
   r.summarize_sil_frequency_phylogeny();
   {
@@ -487,8 +500,11 @@ BOOST_AUTO_TEST_CASE(test_results_example_problem_case)
     f << r.get_summarized_sil_frequency_phylogeny();
     BOOST_TEST_PASSPOINT();
     BOOST_CHECK(is_regular_file(filename_dot));
-    convert_dot_to_svg(filename_dot, filename_svg);
-    convert_svg_to_png(filename_svg, filename_png);
+    if (!"local computer")
+    {
+      convert_dot_to_svg(filename_dot, filename_svg);
+      convert_svg_to_png(filename_svg, filename_png);
+    }
   }
   const auto g = r.get_summarized_sil_frequency_phylogeny();
   /*
--- ribi_simulation_test.cpp
+++ ribi_simulation_test.cpp
@@ -45,8 +45,12 @@ BOOST_AUTO_TEST_CASE(test_do_simulation_simple_run)
   do_simulation(p); //Will abort the test if throws
   BOOST_TEST_PASSPOINT();
   BOOST_REQUIRE(is_regular_file(results_genotype_frequency_graph_filename));
-  convert_dot_to_svg(results_genotype_frequency_graph_filename, "test_do_simulation_simple_run.svg");
-  convert_svg_to_png("test_do_simulation_simple_run.svg", "test_do_simulation_simple_run.png");
+
+  if (!"local computer")
+  {
+    convert_dot_to_svg(results_genotype_frequency_graph_filename, "test_do_simulation_simple_run.svg");
+    convert_svg_to_png("test_do_simulation_simple_run.svg", "test_do_simulation_simple_run.png");
+  }
 }
 
 BOOST_AUTO_TEST_CASE(test_do_simulation_run_example_sim)
@@ -77,8 +81,11 @@ BOOST_AUTO_TEST_CASE(test_do_simulation_run_example_sim)
   do_simulation(p);
   BOOST_TEST_PASSPOINT();
   BOOST_CHECK(is_regular_file(results_genotype_frequency_graph_filename));
-  convert_dot_to_svg(results_genotype_frequency_graph_filename, "test_do_simulation_run_example_sim.svg");
-  convert_svg_to_png("test_do_simulation_run_example_sim.svg", "test_do_simulation_run_example_sim.png");
+  if (!"local computer")
+  {
+    convert_dot_to_svg(results_genotype_frequency_graph_filename, "test_do_simulation_run_example_sim.svg");
+    convert_svg_to_png("test_do_simulation_run_example_sim.svg", "test_do_simulation_run_example_sim.png");
+  }
 }
 
 #pragma GCC diagnostic pop
