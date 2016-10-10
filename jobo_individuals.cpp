9b581ef6f94acd95bc4f845dc9ae5f1179fae221
JorikdeBoer <j.de.boer.40@student.rug.nl>
2016-10-10 09:09:43 +0200

    added point in header file

--- jobo_individuals.cpp
+++ jobo_individuals.cpp
@@ -1,2 +1,20 @@
 #include "jobo_individuals.h"
+#include <iostream>
+#include <stdexcept>
 
+/*
+jobo_individuals::jobo_individuals(
+  const int individual_, const int population_size
+  ): m_n_loci{n_loci}, m_population_size{population_size}
+{
+  if (n_loci < 0)
+  {
+    throw std::invalid_argument("n_loci must be positive");
+  }
+  if (population_size < 0)
+  {
+    throw std::invalid_argument("population_size must be positive");
+  }
+}
+
+*/
--- jobo_simulation.h
+++ jobo_simulation.h
@@ -3,14 +3,16 @@
 
 #include "jobo_parameters.h"
 #include "jobo_individuals.h"
+#include "jobo_individual.h"
 
 class jobo_simulation
 {
 public:
   jobo_simulation(const jobo_parameters& parameters);
+  jobo_parameters get_parameters() const { return m_parameters;}
 
-  jobo_parameters get_parameters() const { return m_parameters; }
- // jobo_individuals get_individuals
+  jobo_simulation(const jobo_individuals& individuals);
+  jobo_individuals get_individuals() const {return m_individual;}
 
 private:
   jobo_parameters m_parameters;
