237e155f58437e61d6b77e7fdba63001215d3eb1
JorikdeBoer <j.de.boer.40@student.rug.nl>
2016-10-12 12:22:26 +0200

    added even number character genotype

--- jobo_individual.cpp
+++ jobo_individual.cpp
@@ -15,12 +15,43 @@ jobo::individual::individual(
 
 int jobo::calc_fitness(std::string genotype)
 {
-    // look if string has 4 characters
+  assert(genotype.size() % 2 == 0); //Odd
+  const int sz{static_cast<int>(genotype.length())};
+  for (int i=0; i!=sz; i+=2)
+  {
+    const char a{genotype[i+0]};
+    const char b{genotype[i+1]};
+    if (std::isupper(a) && std::isupper(b)) return 0;
+  }
+  return 1;
+  /*
+    std::string temp;
+    // look if string has 2,4 or 6 characters
     assert (genotype.size() == 4||genotype.size() == 2||genotype.size() == 6);
 
     // for-loop to take each time 2 characters of the genotype string
-
-    if(std::isupper(genotype[0]) && std::isupper(genotype[1]))
+    const int sz{static_cast<int>(genotype.length())};
+    for ( int i = 0; i != sz; ++i )
+    {
+       // Add the current character to temp to store 2 characters
+       temp += genotype[ i ];
+       if ( temp.length() >= 2 )
+       {
+         // look if both characters are uppercase letters: fitness 0
+         if(std::isupper(temp[0]) && std::isupper(temp[1]))
+         {
+           // returns 0 if both characters are uppercase letters: fitness 0
+           return 0;
+         }
+         // Reset temp
+         temp = "";
+       }
+    }
+    //returns 1 if one of both characters is a lowercase letter: fitness 1
+    return 1;
+  */
+}
+   /* if(std::isupper(genotype[0]) && std::isupper(genotype[1]))
     {
       // returns 0 if both characters are uppercase letters: fitness 0
       return 0;
@@ -31,8 +62,9 @@ int jobo::calc_fitness(std::string genotype)
       return 0;
       }
     //returns 1 if one of both characters is a lowercase letter: fitness 1
-    return 1;
-}
+    return 1;*/
+
+
 
 bool jobo::operator==(const individual& lhs, const individual& rhs) noexcept
 {
