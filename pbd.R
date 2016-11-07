fda70ac8242fb38556cd6ed59cbdcf438eea0391
richelbilderbeek <richel@richelbilderbeek.nl>
2016-11-07 12:51:39 +0100

    Brilliant script-to-be

--- pbd.R
+++ pbd.R
@@ -1,3 +1,33 @@
+# For GUI
+create_plot <- function() {
+
+  library(PBD)
+
+  birth_good <- 0.2
+  completion <- 0.2
+  birth_incipient <- 0.2
+  death_good <- 0.1
+  death_incipient <- 0.1
+  age  <- 15
+  filename <- "pbd.png"
+
+  png(filename)
+  PBD::pbd_sim(
+    pars = c(
+      birth_good,
+      completion,
+      birth_incipient,
+      death_good,
+      death_incipient
+    ),
+    age = age,
+    soc = 2,
+    plotit = TRUE
+  )
+  dev.off()
+
+}
+
 library(PBD)
 
 birth_good <- 0.2
@@ -8,8 +38,7 @@ death_incipient <- 0.1
 age  <- 15
 filename <- "pbd.png"
 
-png(filename)
-PBD::pbd_sim(
+out <- PBD::pbd_sim(
   pars = c(
     birth_good,
     completion,
@@ -18,7 +47,7 @@ PBD::pbd_sim(
     death_incipient
   ),
   age = age,
-  soc = 2,
-  plotit = TRUE
+  soc = 2
 )
-dev.off()
+names(out)
+out$L
