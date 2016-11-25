edf30748b39b9243c6665c32a3387373bbf8cb9b
LykeStuurman <lykestuurman@hotmail.com>
2016-11-25 13:20:56 +0100

    Multiple runs

--- lyke_run.sh
+++ lyke_run.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 module load Qt
 module load GCC/5.1.0
-./lyke
+
+mkdir 1
+cp lyke_peregrine 1
+cd 1
+sbatch ./lyke_peregrine
+cd ..
+
+mkdir 2
+cp lyke_peregrine 2
+cd 2
+sbatch ./lyke_peregrine
+cd ..
+
+mkdir 3
+cp lyke_peregrine 3
+cd 3
+sbatch ./lyke_peregrine
+cd ..
+
