d1e22d36f0b39f4ce0b7a6ab0dc9fa96de8e0352
JamesAndrews91 <james.andrews.2012@gmail.com>
2016-11-28 15:18:25 +0100

    Editted wrong copy

--- checkout_all_branches.sh
+++ checkout_all_branches.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Script to checkout all the branches of the team
-git checkout -b jobo origin/cine
-git checkout -b jobo origin/jaan
+git checkout -b cine origin/cine
+git checkout -b jaan origin/jaan
 git checkout -b jobo origin/jobo
 git checkout -b kewe origin/kewe
 git checkout -b lyke origin/lyke
