d44990282ca74970d0affa924e43c97144d4e984
richelbilderbeek <richel@richelbilderbeek.nl>
2016-11-28 14:12:22 +0100

    Fixed #70

--- checkout_all_branches.sh
+++ checkout_all_branches.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Script to checkout all the branches of the team
+git checkout -b jobo origin/cine
+git checkout -b jobo origin/jaan
 git checkout -b jobo origin/jobo
 git checkout -b kewe origin/kewe
 git checkout -b lyke origin/lyke
