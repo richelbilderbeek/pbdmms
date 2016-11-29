7bb437dec87f25c754a143ad54073eae40ccf0eb
richelbilderbeek <richel@richelbilderbeek.nl>
2016-11-29 11:35:54 +0100

    Jobo GUI is born, crashes due to assert on the index in a vector

---
+++ jobo_qtmaindialog.h
@@ -0,0 +1,48 @@
+#ifndef JOBO_QTMAINDIALOG_H
+#define JOBO_QTMAINDIALOG_H
+
+#include "jobo_parameters.h"
+#include "pbd_ltt.h"
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Weffc++"
+#include <QDialog>
+#pragma GCC diagnostic pop
+
+namespace Ui {
+  class jobo_qtmaindialog;
+}
+
+struct QwtPlot;
+struct QwtPlotCurve;
+
+namespace jobo {
+
+class qtmaindialog : public QDialog
+{
+  Q_OBJECT //!OCLINT
+
+public:
+  explicit qtmaindialog(QWidget *parent = 0);
+  qtmaindialog(const qtmaindialog&) = delete;
+  qtmaindialog& operator=(const qtmaindialog&) = delete;
+  ~qtmaindialog();
+
+  ///Will throw if current values are invalid
+  parameters get_parameters() const;
+
+private slots:
+  void on_button_clicked();
+
+private:
+  Ui::jobo_qtmaindialog *ui;
+
+  QwtPlot * const m_ltt_plot;
+  QwtPlotCurve * m_ltt_plot_line;
+
+  void show_results(const parameters& p);
+  void display_ltt(const pbd::ltt& l);
+};
+
+} //~namespace jobo
+
+#endif // JOBO_QTMAINDIALOG_H
