c70207c99e5e8b6dac008d79273c1b59cee1e37a
S3101835 <s3101835@pc-157-101.fmnsedu.rug.nl>
2016-12-09 12:15:02 +0100

    starting to implement weighted lottery

--- jaan_main.cpp
+++ jaan_main.cpp
@@ -5,32 +5,40 @@
 
     ASSUMPTIONS
     Haploid population
-    Multiple loci with weak increase on ornament and preference.
+    Multiple loci with weak increase on trait and preference.
     Sequential, fixed interval sampling in mate selection.
-    Mate selection prefers bigger ornaments.
+    Mate selection prefers bigger traits.
     Fixed, discrete generations.
 
     From here:
-     - Two different environments
-     - Two way selection of ornament? Or should the ornament be the same and perception is changed?
-     - Environment dependent mortality
-     - Environment dependent selection criteria
-     - Movement between environments and costs for movement
-     - Environment dependent ecological selection
+     - Switch to hermaphrodites
+     - Weighted lottery for who is a mother Vf = exp(-0.5*((p-popt)/deltap)^2) chosen from
+       population based on this distribution.
+     - Weighted lottery for who is a father Vm = exp(-0.5*((t-topt)/deltat)^2) chosen from
+       population based on this distribution.
+     - Mate choice expression exp(p(focal mother) * t(all fathers) * alpha(all fathers), who
+       is best choice is when this expression is maximised but then she chooses based on this
+       distribution.
 */
 
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include <random>
+#include <cmath>
 
-const int gEnd			= 1000;												// Number of Generations to run through.
-const int popSize		= 1000;												// Population size.
-const int nMales		= popSize / 2;										// How many individuals in a vector are treated as males.
-const int nFemales		= popSize - nMales;									// How many individuals in a vector are treated as females.
-const int nPrefGenes	= 10;												// Number of genes for Preference.
-const int nOrnGenes		= 10;												// Number of genes for Ornament.
-const double mu			= 1.0e-4;											// Mutation rate.
+const int gEnd          = 1000;             // Number of Generations to run through.
+const int popSize       = 1000;             // Population size.
+const int nMales        = popSize / 2;      // How many individuals in a vector are treated as males.
+const int nFemales      = popSize - nMales; // How many individuals in a vector are treated as females.
+const int nPrefGenes    = 10;               // Number of genes for Preference.
+const int nTrtGenes     = 10;               // Number of genes for Trait.
+const double traitCost  = 0.0;              // How dangerous it is to have an Trait.
+const double pOpt       = 0.0;              // Optimal preference value for viability.
+const double tOpt       = 0.0;              // Optimal trait value for viability.
+const double deltap     = 0.0;              // Cost of choosiness.
+const double deltat     = 0.0;              // Cost of trait.
+const double mu         = 1.0e-4;           // Mutation rate.
 std::default_random_engine generator;
 std::uniform_real_distribution<double> distribution(0.0, 1.0);
 
@@ -46,21 +54,21 @@ class Individual
 public:
     Individual();
     Individual(const Individual&, const Individual&);
-    //Individual(const Individual&);
-    //Individual& operator=(const Individual&);
     void mateSelect(const std::vector<Individual>&, const int);
-    void mortality(const double ornamentCost);
+    void mortality(const double traitCost);
     double getPref();
-    double getOrn();
+    double getTrt();
     int getMate();
     bool alive;
 private:
     void mutate();
     void develop();
     std::vector<double> prefGenes;
-    std::vector<double> ornGenes;
+    std::vector<double> trtGenes;
     double preference;
-    double ornament;
+    double trait;
+    double vFemale;
+    double vMale;
     int mate;
 };
 
@@ -71,20 +79,22 @@ Individual::Individual() :
 // Initialisation constructor.
 alive(true),
 prefGenes(nPrefGenes),
-ornGenes(nOrnGenes),
+trtGenes(nTrtGenes),
 preference(0.0),
-ornament(0.0),
+trait(0.0),
+vFemale(0.0),
+vMale(0.0),
 mate(-1)
 {
-    for (int i = 0; i < nOrnGenes; ++i) {									// Randomly set each gene to 0 or 1.
+    for (int i = 0; i < nTrtGenes; ++i) {       // Randomly set each gene to 0 or 1.
         if (distribution(generator) < 0.2) {
-            ornGenes[i] = 0;
+            trtGenes[i] = 0;
         }
         else {
-            ornGenes[i] = 1;
+            trtGenes[i] = 1;
         }
     }
-    for (int i = 0; i < nPrefGenes; ++i) {									// Randomly set each Preference gene to 0 or 1.
+    for (int i = 0; i < nPrefGenes; ++i) {      // Randomly set each Preference gene to 0 or 1.
         if (distribution(generator) < 0.8) {
             prefGenes[i] = 0;
         }
@@ -100,20 +110,22 @@ Individual::Individual(const Individual& mother, const Individual& father) :
 // Reproduction constructor.
 alive(true),
 prefGenes(nPrefGenes),
-ornGenes(nOrnGenes),
+trtGenes(nTrtGenes),
 preference(0.0),
-ornament(0.0),
+trait(0.0),
+vFemale(0.0),
+vMale(0.0),
 mate(-1)
 {
-    for (int i = 0; i < nOrnGenes; ++i) {									// Inherit mother and father genes randomly 50:50.
+    for (int i = 0; i < nTrtGenes; ++i) {          // Inherit mother and father genes randomly 50:50.
         if (distribution(generator) < 0.5) {
-            ornGenes[i] = mother.ornGenes[i];
+            trtGenes[i] = mother.trtGenes[i];
         }
         else {
-            ornGenes[i] = father.ornGenes[i];
+            trtGenes[i] = father.trtGenes[i];
         }
     }
-    for (int i = 0; i < nPrefGenes; ++i) {									// Inherit mother and father preference genes randomly 50:50.
+    for (int i = 0; i < nPrefGenes; ++i) {         // Inherit mother and father preference genes randomly 50:50.
         if (distribution(generator) < 0.5) {
             prefGenes[i] = mother.prefGenes[i];
         }
@@ -131,12 +143,12 @@ void Individual::mateSelect(const std::vector<Individual>& population, const int
     Takes a vector of all individuals and selects randomly from the second half of the vector.
     Function returns -1 if no suitable mate is found, otherwise returns the position of the male. */
 {
-    for (double t = 0.0, d = nMales; t < d; ++t) {							// Sample the males at random.
+    for (double t = 0.0; t < nMales; ++t) {                                     // Sample the males at random.
         std::uniform_int_distribution<int> pickMan(0, nMales - deadMales - 1);
         int focal = nMales + pickMan(generator);
 
-        if (population[focal].ornament >= preference) {
-            mate = focal;													// If the male is good, return his position in vector.
+        if (population[focal].trait >= preference) {
+            mate = focal;                                                       // If the male is good, return his position in vector.
         }
     }
     if (mate == -1) {
@@ -144,10 +156,10 @@ void Individual::mateSelect(const std::vector<Individual>& population, const int
     }
 }
 
-void Individual::mortality(const double ornamentCost)
-// Causes mortality for males based on ornament cost.
+void Individual::mortality(const double traitCost)
+// Causes mortality for males based on trait cost.
 {
-    if (ornament * ornamentCost > distribution(generator)) {
+    if (trait * traitCost > distribution(generator)) {
         alive = false;
     }
 }
@@ -162,21 +174,21 @@ double Individual::getPref()
     return preference;
 }
 
-double Individual::getOrn()
+double Individual::getTrt()
 {
-    return ornament;
+    return trait;
 }
 
 //PRIVATE INDIVIDUAL CLASS FUNCTIONS
 void Individual::mutate()
 // Give each gene a chance of flipping.
 {
-    for (int i = 0; i < nOrnGenes; ++i) {
+    for (int i = 0; i < nTrtGenes; ++i) {
         if (distribution(generator) < mu)  {
-            if (ornGenes[i] == 1) {
-                ornGenes[i] = 0;
+            if (trtGenes[i] == 1) {
+                trtGenes[i] = 0;
             }
-            else ornGenes[i] = 1;
+            else trtGenes[i] = 1;
         }
     }
     for (int i = 0; i < nPrefGenes; ++i) {
@@ -190,18 +202,22 @@ void Individual::mutate()
 }
 
 void Individual::develop()
-/*	Calculate preference from genes 1 to nPreferenceGenes.
-    Calculate ornament from genes nPreferenceGenes to nGenes.*/
+/*	Calculate preference from prefGenes.
+    Calculate trait from trtGenes.*/
 {
     for (int i = 0; i < nPrefGenes; ++i) {
         preference += prefGenes[i];
     }
     preference /= nPrefGenes;
+    double temp = (preference - pOpt) / deltap;
+    vFemale = exp(-0.5 * temp * temp);
 
-    for (int i = 0; i < nOrnGenes; ++i) {
-        ornament += ornGenes[i];
+    for (int i = 0; i < nTrtGenes; ++i) {
+        trait += trtGenes[i];
     }
-    ornament /= nOrnGenes;
+    trait /= nTrtGenes;
+    temp = (trait - tOpt) / deltat;
+    vMale = exp(-0.5 * temp * temp);
 }
 
 //
@@ -285,108 +301,79 @@ int main()
     // Set up initial population.
     std::vector<Individual> population(popSize);
 
-    std::ofstream output("data.csv");
+    //	Enter generational For loop.
+    for (int g = 0; g < gEnd; ++g) {
 
-    // Enter ornamentCost for loop.
-    for (double ornamentCost = 0.0; ornamentCost < 1.1; ornamentCost += 1.1) {
-        // Enter repetition for loop.
-        for (int r = 0; r < 1; ++r) {
-            //	Enter generational For loop.
-//            std::cout << "Cost\tR\tG\tPref\tOrn\n";
-//            output << "Cost\tR\tG\tPref\tOrn\n";
-            for (int g = 0; g < gEnd; ++g) {
-
-                // Mortality of males (second half of the population) by ornament size.
-                for (int i = nMales; i < popSize; ++i) {
-                    population[i].mortality(ornamentCost);
-                }
-                // Rearrange Males and Females in order of living and dead.
-                int deadMales = 0;
-                int deadFemales = 0;
-                if (livingPopulation(deadMales, deadFemales, population) == false) {
-                    std::cerr << " in generation " << g << '\n';
-                    terminateProgram();
-                    exit(1);
-                }
-                arrangeFemales(deadFemales, population);
-                arrangeMales(deadMales, population);
-
-                // Female mate choice.
-                // Females select mates and those that fail to reproduce are moved to the end of the vector.
-                for (int i = 0; i < nFemales - deadFemales; ++i) {
-                    population[i].mateSelect(population, deadMales);
-                }
-
-                // Rearrange population once more, to ensure that females that didn't reproduce are at the end. important for when females don't sample the entire population.
-                deadMales = 0;
-                deadFemales = 0;
-                if (livingPopulation(deadMales, deadFemales, population) == false) {
-                    std::cerr << " in generation " << g << '\n';
-                    terminateProgram();
-                    exit(1);
-                }
-                arrangeFemales(deadFemales, population);
-                arrangeMales(deadMales, population);
-
-                // Matings
-                std::vector<Individual> girls;
-                std::vector<Individual> boys;
-                for (int i = 0; i < nFemales; ++i) {
-                    // Set the mother to the current female and the father to the chosen mate of the female.
-                    int mother = i;
-                    int father = population[i].getMate();
-                    // If the mother is dead, choose a random female from the living females and her partner.
-                    if (population[i].alive == 0) {
-                        std::uniform_int_distribution<int> pickWoman(0, nFemales - deadFemales - 1);
-                        mother = pickWoman(generator);
-                        father = population[mother].getMate();
-                    }
-                    if (father < 0) {
-                        std::cerr << "PROBLEM WITH MATING\n";
-                    }
-                    // Produce two offspring and attach one each to the vectors for males and females.
-                    Individual girl(population[mother], population[father]);
-                    Individual boy(population[mother], population[father]);
-                    girls.push_back(girl);
-                    boys.push_back(boy);
-                }
-                // Set up the next population in this vector.
-                std::vector<Individual> offspring;
-                for (int i = 0; i < nFemales; ++i) {
-                    offspring.push_back(girls[i]);
-                }
-                for (int i = 0; i < nMales; ++i) {
-                    offspring.push_back(boys[i]);
-                }
-
-                // Calculate average ornament and preference in the population.
-                double avPref = 0.0;
-                for (int i = 0; i < nFemales; ++i) {
-                    avPref += population[i].getPref();
-                }
-                avPref /= nFemales;											// Calculate the average preference of females in this generation.
-                double avOrn = 0.0;
-                for (int i = nMales; i < popSize; ++i) {
-                    avOrn += population[i].getOrn();
-                }
-                avOrn /= nMales;											// Calculate the average ornamentation of males in this generation.
-//                std::cout << ornamentCost << '\t' << r << '\t' << g << '\t' << avPref << '\t' << avOrn << '\n';
-//                output << ornamentCost << '\t' << r << '\t' << g << '\t' << avPref << '\t' << avOrn << '\n';
-
-                // Set offspring as the current generation.
-                for (int i = 0; i < popSize; ++i) {
-                    population[i] = offspring[i];
-                }
-            }
-//            std::cout << "\n\n";
-//            output << "\n\n";
+        // Mortality of males (second half of the population) by trait size.
+        for (int i = nMales; i < popSize; ++i) {
+            population[i].mortality(traitCost);
         }
-//        std::cout << "\n\n";
-//        output << "\n\n";
+        // Rearrange Males and Females in order of living and dead.
+        int deadMales = 0;
+        int deadFemales = 0;
+        if (livingPopulation(deadMales, deadFemales, population) == false) {
+            std::cerr << " in generation " << g << '\n';
+            terminateProgram();
+            exit(1);
+        }
+        arrangeFemales(deadFemales, population);
+        arrangeMales(deadMales, population);
+
+        // Female mate choice.
+        // Females select mates and those that fail to reproduce are moved to the end of the vector.
+        for (int i = 0; i < nFemales - deadFemales; ++i) {
+            population[i].mateSelect(population, deadMales);
+        }
+
+        // Rearrange population once more, to ensure that females that didn't reproduce are at the end. important for when females don't sample the entire population.
+        deadMales = 0;
+        deadFemales = 0;
+        if (livingPopulation(deadMales, deadFemales, population) == false) {
+            std::cerr << " in generation " << g << '\n';
+            terminateProgram();
+            exit(1);
+        }
+        arrangeFemales(deadFemales, population);
+        arrangeMales(deadMales, population);
+
+        // Matings
+        std::vector<Individual> girls;
+        std::vector<Individual> boys;
+        for (int i = 0; i < nFemales; ++i) {
+            // Set the mother to the current female and the father to the chosen mate of the female.
+            int mother = i;
+            int father = population[i].getMate();
+            // If the mother is dead, choose a random female from the living females and her partner.
+            if (population[i].alive == 0) {
+                std::uniform_int_distribution<int> pickWoman(0, nFemales - deadFemales - 1);
+                mother = pickWoman(generator);
+                father = population[mother].getMate();
+            }
+            if (father < 0) {
+                std::cerr << "PROBLEM WITH MATING\n";
+            }
+            // Produce two offspring and attach one each to the vectors for males and females.
+            Individual girl(population[mother], population[father]);
+            Individual boy(population[mother], population[father]);
+            girls.push_back(girl);
+            boys.push_back(boy);
+        }
+        // Set up the next population in this vector.
+        std::vector<Individual> offspring;
+        for (int i = 0; i < nFemales; ++i) {
+            offspring.push_back(girls[i]);
+        }
+        for (int i = 0; i < nMales; ++i) {
+            offspring.push_back(boys[i]);
+        }
+
+        // Set offspring as the current generation.
+        for (int i = 0; i < popSize; ++i) {
+            population[i] = offspring[i];
+        }
     }
 
     // Terminate Program.
-    output.close();
     terminateProgram();
     return 0;
 }
