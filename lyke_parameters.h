8d52a2da5f246a87264e21daf939508af8ae2e2f
LykeStuurman <lykestuurman@hotmail.com>
2016-12-09 13:58:14 +0100

    simulation8

--- lyke_parameters.h
+++ lyke_parameters.h
@@ -11,7 +11,7 @@ class lyke_parameters
 {
 public:
   lyke_parameters(
-    const int    simulationruns = 5000,
+    const int    simulationruns = 10000,
     const int    L = 120,
     const int    nGeneEco = 10,
     const double mu = 0.001,
