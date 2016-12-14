f3f2a5b027fb57ac4a3427c72c9de6e39f9a1fc3
EllyJet <ellesjet@gmail.com>
2016-12-14 17:11:15 +0100

    added variables class functions

--- elly_variables.cpp
+++ elly_variables.cpp
@@ -8,7 +8,7 @@ elly::variables::variables(
     double time,
     int main_init)
   : m_species_in_clades(main_init),
-    m_time{0.0},
+    m_time{time},
     m_main_init{500},
     m_id_counter{0},
     m_all_species_mainland(main_init),
@@ -17,4 +17,47 @@ elly::variables::variables(
 {
   if(time < 0.0)
     throw std::invalid_argument("time can not be negative");
+}
+
+
+void iterate_time(const double waiting_time)
+{
+  if(waiting_time < 0.0)
+    throw std::invalid_argument("waiting time must be positive");
+  m_time += waiting_time;
+}
+
+void add_species_mainland(const species newspecies)
+{
+  m_all_species_mainland.push_back(newspecies);
+}
+
+void add_species_island(const species newspecies)
+{
+  m_all_island_species.push_back(newspecies);
+}
+
+void add_species_both(const species newspecies)
+{
+  m_all_species_both.push_back(newspecies);
+}
+
+void remove_species_mainland(const int i){
+  elly::species temp =  m_all_species_mainland.end();
+  m_all_species_mainland[i] = temp;
+  m_all_species_mainland.pop_back();
+}
+
+void remove_species_island(const int i)
+{
+  elly::species temp =  m_all_species_island.end();
+  m_all_species_island[i] = temp;
+  m_all_species_island.pop_back();
+}
+
+void remove_species_both(const int i)
+{
+  elly::species temp =  m_all_species_both.end();
+  m_all_species_both[i] = temp;
+  m_all_species_both.pop_back();
 }
--- elly_variables.h
+++ elly_variables.h
@@ -22,6 +22,17 @@ public:
     std::vector<species> m_all_species_mainland;
     std::vector<species> m_all_species_island;
     std::vector<species> m_all_species_both;
+
+    double get_time() const noexcept {return m_time; }
+    int get_id_counter() const noexcept { return m_id_counter; }
+    void iterate_time(const double waiting_time);
+    void iterate_id_counter(){ m_id_counter += 1;}
+    void add_species_mainland(const species newspecies);
+    void add_species_island(const species newspecies);
+    void add_species_both(const species newspecies);
+    void remove_species_mainland(const int i);
+    void remove_species_island(const int i);
+    void remove_species_both(const int i);
 };
 
 } //~namespace elly
