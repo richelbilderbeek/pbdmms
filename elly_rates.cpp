7a85ae45e1d9d16954db9e7d9068a764ab94fa13
richelbilderbeek <richel@richelbilderbeek.nl>
2016-12-14 09:16:02 +0100

    No OCLint here

--- elly_rates.cpp
+++ elly_rates.cpp
@@ -4,7 +4,7 @@
 
 elly::rates::rates()
   : mclad{0.0}, mext{0.0}, mimm{0.0} , iext{0.0} , iclad{0.0} , iimm{0.0} , bextm{0.0},
-    bexti{0.0}, bana{0.0}, bcladi{0.0} , bcladm{0.0}
+    bexti{0.0}, bana{0.0}, bcladi{0.0} , bcladm{0.0} //!OCLINT It would only make the code more complex if parameters are seperated by location
 {
 
 
