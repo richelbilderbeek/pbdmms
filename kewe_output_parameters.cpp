e5e9f5197f1f7e07de4a0a625bc559972542c669
Lumphie <wesselinkkees@gmail.com>
2016-12-23 12:21:29 +0100

    added output_parameters operator

--- kewe_output_parameters.cpp
+++ kewe_output_parameters.cpp
@@ -5,8 +5,14 @@
 std::ostream& kewe::operator<<(std::ostream& os, const output_parameters p) noexcept
 {
   os
-    << "STUB"
-    << p.histbinp
-  ;
+    << "outputfrequency: " << p.outputfreq << '\n'
+    << "histw: " << p.histw << '\n'
+    << "histbinx: " << p.histbinx << '\n'
+    << "histbinp: " << p.histbinp << '\n'
+    << "histbinq: " << p.histbinq << '\n'
+    << "outputfilename: " << p.outputfilename << '\n'
+    << "ltt_plot_filename: " << p.ltt_plot_filename << '\n'
+    << "is silent: " << static_cast<bool>(p.is_silent);
+
   return os;
 }
