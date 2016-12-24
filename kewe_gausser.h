ed0e7f16ec83807f7d8db32cd15b95e71111d400
richelbilderbeek <richel@richelbilderbeek.nl>
2016-12-25 00:22:06 +0100

    Use gausser_impl_2

--- kewe_gausser.h
+++ kewe_gausser.h
@@ -1,11 +1,11 @@
 #ifndef KEWE_GAUSSER_H
 #define KEWE_GAUSSER_H
 
-#include "gausser.h"
+#include "gausser_impl_2.h"
 
 namespace kewe {
 
-using gausser = ::ribi::gausser;
+using gausser = ::ribi::gausser_impl_2;
 
 } //~namespace kewe
 
