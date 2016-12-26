13deec898cde8b90cdd08c6fa7a48cd71c3756dd
richelbilderbeek <richel@richelbilderbeek.nl>
2016-12-26 20:09:54 +0100

    For giggles, use std:vector

--- sado_population.h
+++ sado_population.h
@@ -2,13 +2,13 @@
 #define SADO_POPULATION_H
 
 #include <list>
-//#include <vector>
+#include <vector>
 #include "sado_individual.h"
 
 namespace sado {
 
-using population = std::list<indiv>;
-//using population = std::vector<indiv>;
+//using population = std::list<indiv>;
+using population = std::vector<indiv>;
 extern population pop; //GLOBAL
 using  my_iterator = population::iterator;
 
