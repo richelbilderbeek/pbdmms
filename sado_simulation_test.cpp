9ef65c490d78953a9c360d308c255a64d0f44b90
richelbilderbeek <richel@richelbilderbeek.nl>
2016-12-30 17:39:38 +0100

    Test for Golden Standard in sado_test

--- sado_simulation_test.cpp
+++ sado_simulation_test.cpp
@@ -1,6 +1,8 @@
 #include "sado_simulation.h"
 
+#include <fstream>
 
+#include "sado_helper.h"
 #include "sado_parameters.h"
 
 // Boost.Test does not play well with -Weffc++
@@ -12,12 +14,36 @@ using namespace sado;
 
 BOOST_AUTO_TEST_CASE(sado_simulation_must_reproduce_golden_standard)
 {
-  /*
+  const std::string filename{"sado_simulation_must_reproduce_golden_standard.txt"};
   const auto p = create_golden_standard_parameters();
   simulation s(p);
   s.run();
-  BOOST_CHECK_EQUAL(s.get_results(), get_golden_standard_results());
-  */
+  {
+    std::ofstream f(filename);
+    f << s.get_results();
+  }
+  auto measured = file_to_vector(filename);
+  assert(measured.back() == "");
+  measured.pop_back();
+  const auto expected = get_golden_output();
+  BOOST_CHECK_EQUAL(measured.size(), expected.size());
+
+  if (measured != get_golden_output())
+  {
+    std::cerr << "ERROR\n";
+    const int sz{static_cast<int>(std::min(measured.size(), expected.size()))};
+    for (int i=0; i!=sz; ++i)
+    {
+      std::cerr
+        << "expected[" << i << "]:" << '\n'
+        << expected[i] << '\n'
+        << "measured[" << i << "]:" << '\n'
+        << measured[i] << '\n'
+      ;
+    }
+
+  }
+  BOOST_CHECK(measured == expected);
 }
 
 #pragma GCC diagnostic pop
