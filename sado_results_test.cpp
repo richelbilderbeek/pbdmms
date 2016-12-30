9e6ce5b8ed58c28291369f37f8b64830237d23eb
richelbilderbeek <richel@richelbilderbeek.nl>
2016-12-30 14:49:19 +0100

    Write sado_create_header_str, fixed #175

--- sado_results_test.cpp
+++ sado_results_test.cpp
@@ -13,16 +13,10 @@ BOOST_AUTO_TEST_CASE(sado_get_golden_standard_results)
   //const auto r = get_golden_standard_results();
 }
 
-
-BOOST_AUTO_TEST_CASE(sado_create_header)
+BOOST_AUTO_TEST_CASE(sado_create_header_str)
 {
   const auto p = create_golden_standard_parameters();
-  create_header(p);
-  const std::string filename = p.get_output_filename();
-  const auto lines = file_to_vector(filename);
-  BOOST_CHECK_EQUAL(lines.size(), 2);
-  BOOST_CHECK_EQUAL(lines[1], std::string());
-  const std::string measured{lines[0]};
+  const std::string measured{create_header_str(p)};
   const std::string expected{get_golden_output()[0]};
   BOOST_CHECK_EQUAL(measured, expected);
 }
