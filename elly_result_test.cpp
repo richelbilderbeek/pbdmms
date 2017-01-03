3eb398e8b72fb8dd5a95947089bbee3c63d6d5c5
EllyJet <ellesjet@gmail.com>
2017-01-03 14:15:52 +0100

    added result_tests

--- elly_result_test.cpp
+++ elly_result_test.cpp
@@ -11,9 +11,9 @@ using namespace elly;
 
 BOOST_AUTO_TEST_CASE(elly_scenario_1)
 {
-  const species_id parent_id = create_new_species_id();
+  const species_id pid = create_new_species_id();
   const species_id id = create_new_species_id();
-  const clade_id ci = create_new_clade_id();
+  const clade_id cid = create_new_clade_id();
   const double time_of_birth{1.1};
   const double time_migrate_to_island{5.1};
   const double time_extinction_mainland{7.8};
@@ -28,10 +28,56 @@ BOOST_AUTO_TEST_CASE(elly_scenario_1)
 
   std::stringstream expected;
   expected << id << ',' << pid << ',' << time_of_birth << ',' << -1.0 << ','
-    << time_extinction_mainland << ',' << -1.0 << ',' << time_migrate_to_island
+    << time_extinction_mainland << ',' << -1.0 << ',' << time_migrate_to_island << ',' << cid;
   ;
 
   BOOST_CHECK_EQUAL(measured.str(), expected.str())    ;
+}
+
+BOOST_AUTO_TEST_CASE(elly_scenario_2)
+{
+  const species_id parent_id = create_new_species_id();
+  const species_id id = create_new_species_id();
+  const clade_id ci = create_new_clade_id();
+  const double time_of_birth{2.5};
+  const double time_migrate_to_island{6.2};
+  const double time_extinction_island{8.5};
+
+  species s(id, pid, cid, location::mainland, time_of_birth);
+  s.migrate_to_island(time_migrate_to_island);
+  s.go_extinct(time_extinction_island, location::island);
+
+  const result r(s);
+  std::stringstream measured;
+  measured << r;
+
+  std::stringstream expected;
+  expected << id << ',' << pid << ',' << time_of_birth << ',' << -1.0 << ',' << -1.0 << ',' << -1.0
+           << ',' << time_extinction_island << ',' << time_migrate_to_island << ',' << cid
+  ;
+
+  BOOST_CHECK_EQUAL(measured.str() , expected.str())    ;
+}
+
+BOOST_AUTO_TEST_CASE(elly_scenario_3)
+{
+  const species_id parent_id = create_new_species_id();
+  const species_id id = create_new_species_id();
+  const clade_id cid = create_new_clade_id();
+  const double time_of_birth{3.2};
+
+  species s(id, pid, cid, location::island, time_of_birth);
+
+  const result r(S);
+  std::stringstream measured;
+  measured << r;
+
+  std::stringstream expected;
+  expected << id << ',' << pid << ',' << -1.0 << ',' << time_of_birth << ',' << -1.0 << ',' << -1.0 <<  ','
+              << -1.0 << ',' << -1.0 << ',' << cid
+  ;
+
+  BOOST_CHECK_EQUAL(measured.str() , expected.str())     ;
 }
 
 #pragma GCC diagnostic pop
