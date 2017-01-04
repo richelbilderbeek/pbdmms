3ec4acfdf70eae9c3456c4dfec53d37095f85ace
EllyJet <ellesjet@gmail.com>
2017-01-04 12:12:37 +0100

    removed simulation test, too slow

--- elly_simulation_test.cpp
+++ elly_simulation_test.cpp
@@ -15,13 +15,13 @@ BOOST_AUTO_TEST_CASE(elly_run_simulation)
   simulation s(p);
   BOOST_CHECK_NO_THROW(s.run());
 }
-
+/*
 BOOST_AUTO_TEST_CASE(elly_run_simulation_with_profiling_parameters)
 {
   const parameters p = create_profiling_parameters();
   simulation s(p);
   BOOST_CHECK_NO_THROW(s.run());
 }
-
+*/
 
 #pragma GCC diagnostic pop
