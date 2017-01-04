c92c1a3eb31205c1a7ed2d49b4761efee14da3c5
richelbilderbeek <richel@richelbilderbeek.nl>
2017-01-04 14:21:21 +0100

    Profile less

--- elly_main.cpp
+++ elly_main.cpp
@@ -1,11 +1,12 @@
-#include <cassert>
-#include <iostream>
 #include "elly_parameters.h"
 #include "elly_event_rates.h"
 #include "elly_species.h"
 #include "elly_gillespie.h"
 #include "elly_events.h"
 #include "elly_simulation.h"
+#include <cassert>
+#include <fstream>
+#include <iostream>
 #include <vector>
 #include <random>
 #include <chrono>
@@ -28,6 +29,8 @@ int main(int argc, char* argv[])
     const parameters p = create_parameters_set1();
     simulation s(p);
     s.run();
+    std::ofstream f("elly_output.csv");
+    f << get_results(s) << '\n';
     std::cout << get_results(s) << '\n';
   }
   catch (std::exception& e)
