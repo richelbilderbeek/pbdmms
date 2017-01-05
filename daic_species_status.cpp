a1b03ef054081ea13c5de675e53b8cec41ed7b71
EllyJet <ellesjet@gmail.com>
2017-01-05 10:12:51 +0100

    added function species_status and tests

--- daic_species_status.cpp
+++ daic_species_status.cpp
@@ -1,2 +1,31 @@
 #include "daic_species_status.h"
+#include <string>
+#include <stdexcept>
 
+std::string daic::to_str(const species_status s)
+{
+  if(s == species_status::endemic)
+    return "Endemic";
+  if(s == species_status::non_endemic)
+    return "Non_endemic";
+  if(s == non_endemic_max_age)
+    return "Non_endemic_MaxAge";
+  throw std::exception("could not convert species_status to string");
+}
+
+daic::species_status daic::to_species_status(const std::string& s)
+{
+  if(s == "Endemic")
+    return species_status::endemic;
+  if(s == "Non_endemic")
+    return species_status::non_endemic;
+  if(s == "Non_endemic_MaxAge")
+    return species_status::non_endemic_max_age;
+  throw std::exception("could not convert string to species_status");
+}
+
+std::ostream& daic::operator<<(std::ostream& os, const species_status& r) noexcept
+{
+  os << to_str(r);
+  return os;
+}
--- daic_species_status_test.cpp
+++ daic_species_status_test.cpp
@@ -4,12 +4,36 @@
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Weffc++"
 #include <boost/test/unit_test.hpp>
+#include <string>
 
 using namespace daic;
 
 BOOST_AUTO_TEST_CASE(daic_to_str_and_to_species_status_must_be_symmetrical)
 {
-  BOOST_CHECK()
+  {
+    species_status s = species_status::endemic;
+    BOOST_CHECK_EQUAL(to_str(s), "Endemic");
+  }
+  {
+    species_status s = species_status::non_endemic;
+    BOOST_CHECK_EQUAL(to_str(s), "Non_endemic");
+  }
+  {
+    species_status s = species_status::non_endemic_max_age;
+    BOOST_CHECK_EQUAL(to_str(s), "Non_endemic_Max_age");
+  }
+  {
+    std::string str = "Endemic";
+    BOOST_CHECK_EQUAL(to_species_status(str), species_status::endemic);
+  }
+  {
+    std::string str = "Non_endemic";
+    BOOST_CHECK_EQUAL(to_species_status(str), species_status::non_endemic);
+  }
+  {
+    std::string str = "Non_endemic_Max_age";
+    BOOST_CHECK_EQUAL(to_species_status(str), species_status::non_endemic_max_age);
+  }
 }
 
 
