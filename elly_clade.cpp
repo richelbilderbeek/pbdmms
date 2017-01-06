da617f57eeb57f2bdb83e71a51a5deb0e3980819
EllyJet <ellesjet@gmail.com>
2017-01-06 14:36:18 +0100

    used algorithm in same clade function

--- elly_clade.cpp
+++ elly_clade.cpp
@@ -1,6 +1,8 @@
 #include "elly_clade.h"
 
 #include <cassert>
+#include <vector>
+#include <iterator>
 
 #include "elly_species.h"
 
@@ -23,6 +25,11 @@ bool elly::all_have_same_clade_id(const std::vector<species>& s)
 {
   assert(!s.empty());
   species first = s[0];
+
+  return std::all_of(s.begin(), s.end(),
+    [] (const species t)
+    { return t.get_clade_id() == first.get_clade_id(); });
+  /*
   for(const species next: s)
   {
     if(first.get_clade_id() != next.get_clade_id())
@@ -31,6 +38,7 @@ bool elly::all_have_same_clade_id(const std::vector<species>& s)
     }
   }
   return true;
+  */
 }
 
 elly::clade_id elly::clade::get_id() const noexcept
