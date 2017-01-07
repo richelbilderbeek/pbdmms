054d604689d9484b21bae85343ccf6d7c03bf6e5
richelbilderbeek <richel@richelbilderbeek.nl>
2017-01-07 18:07:25 +0100

    Increase codecov

--- elly_clade_test.cpp
+++ elly_clade_test.cpp
@@ -9,20 +9,69 @@
 
 using namespace elly;
 
+BOOST_AUTO_TEST_CASE(elly_clade_construction)
+{
+  //Can create an empty clade
+  {
+    const std::vector<species> v = {};
+    BOOST_CHECK_NO_THROW(clade c(v));
+  }
+  //Can create a clade with one species
+  {
+    const std::vector<species> v
+      = { create_new_test_species(location::mainland) };
+    BOOST_CHECK_NO_THROW(clade c(v));
+  }
+  //Can create a clade with two species of same clade ID
+  {
+    const species a = create_new_test_species(location::mainland);
+    const species b = create_descendant(a, 1.0, location::mainland);
+    const std::vector<species> v = { a, b };
+    BOOST_CHECK_NO_THROW(clade c(v));
+  }
+  //Cannot create a clade with two species with different clade IDs
+  {
+    const species a = create_new_test_species(location::mainland);
+    const species b = create_new_test_species(location::mainland);
+    const std::vector<species> v = { a, b };
+    assert(a.get_clade_id() != b.get_clade_id());
+    BOOST_CHECK_THROW(clade c(v), std::invalid_argument);
+  }
+}
+
 BOOST_AUTO_TEST_CASE(elly_all_have_same_clade_id)
 {
-  //Create two clades:
-  // a -- b -- c
-  //
-  // d
+  {
+    //Create two clades:
+    // a -- b -- c
+    //
+    // d
+    const species a = create_new_test_species(location::mainland);
+    const species b = create_descendant(a, 1.0, location::mainland);
+    const species c = create_descendant(b, 1.0, location::mainland);
+    const species d = create_new_test_species(location::mainland);
+    BOOST_CHECK(all_have_same_clade_id( {a, b, c} ));
+    BOOST_CHECK(all_have_same_clade_id( {d} ));
+    BOOST_CHECK(!all_have_same_clade_id( {a, d} ));
+  }
+  //Cannot measure when clade is empty
+  {
+    const std::vector<species> v = {};
+    const clade c(v);
+    BOOST_CHECK_THROW(c.get_id(), std::logic_error);
+  }
+}
+
+BOOST_AUTO_TEST_CASE(elly_get_ancestor)
+{
   const species a = create_new_test_species(location::mainland);
   const species b = create_descendant(a, 1.0, location::mainland);
-  const species c = create_descendant(b, 1.0, location::mainland);
-  const species d = create_new_test_species(location::mainland);
-  BOOST_CHECK(all_have_same_clade_id( {a, b, c} ));
-  BOOST_CHECK(all_have_same_clade_id( {d} ));
-  BOOST_CHECK(!all_have_same_clade_id( {a, d} ));
+  const std::vector<species> v = { a, b };
+  const clade c(v);
+  BOOST_CHECK(a == get_ancestor(b, c));
+  BOOST_CHECK_THROW(get_ancestor(a, c), std::invalid_argument);
 }
+
 
 #pragma GCC diagnostic pop
 
