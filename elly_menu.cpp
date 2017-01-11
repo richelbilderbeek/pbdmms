020335baf2f5ae3ba569245758be3761d904fd97
richelbilderbeek <richel@richelbilderbeek.nl>
2017-01-11 09:29:32 +0100

    elly: show header in CLI output

--- elly_menu.cpp
+++ elly_menu.cpp
@@ -105,8 +105,10 @@ void elly::run_from_file(
 
   std::cout
     << "DAISIE estimates, ideal:\n"
+    << get_output_header() << '\n'
     << e.get_output_ideal() << '\n'
     << "DAISIE estimates, reality:\n"
+    << get_output_header() << '\n'
     << e.get_output_reality() << '\n'
   ;
 }
