766ec1502e0398d12b86b610ebe773c618688e0c
richelbilderbeek <richel@richelbilderbeek.nl>
2017-01-18 09:24:31 +0100

    Show version of R packages [skip ci]

--- install_r_packages.R
+++ install_r_packages.R
@@ -4,3 +4,8 @@ devtools::install_github("richelbilderbeek/DAISIE")
 devtools::install_github("richelbilderbeek/nLTT")
 devtools::install_github("rsetienne/PBD", subdir = "PBD")
 warnings()
+
+packageVersion("DAISIE")
+packageVersion("devtools")
+packageVersion("nLTT")
+packageVersion("PBD")
