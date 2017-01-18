ba69a23676eef67ad488b3825fb56b054690c91a
Lumphie <wesselinkkees@gmail.com>
2017-01-18 15:10:20 +0100

    added jobscript

---
+++ sado_jobscript1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --time=8-12:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks=1
+#SBATCH --mem=100G
+#SBATCH --job-name=kewe_sado_I
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-user=k.w.wesselink@student.rug.nl
+#SBATCH --output=kewe_sado_I.log
+module load Qt
+module load GCC/5.1.0
+.jsado_peregrine_2 ../kewe_sado_test_parameters.txt
