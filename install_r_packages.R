d5b23d066f89958d1ece5276742607fd149d225a
richelbilderbeek <richel@richelbilderbeek.nl>
2017-01-18 09:19:27 +0100

    Use DAISIE v1.3

--- install_r_packages.R
+++ install_r_packages.R
@@ -1,6 +1,6 @@
 # Installs the R packages needed
-install.packages("DAISIE", repos = "http://cran.uk.r-project.org")
 install.packages("devtools", repos = "http://cran.uk.r-project.org")
+devtools::install_github("richelbilderbeek/DAISIE")
 devtools::install_github("richelbilderbeek/nLTT")
 devtools::install_github("rsetienne/PBD", subdir = "PBD")
 warnings()
