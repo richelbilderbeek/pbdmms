d222cbfe616a3b48675be600870c432c7adf3689
Lumphie <wesselinkkees@gmail.com>
2017-01-19 11:18:43 +0100

    returned histw back to 50

--- sado_parameters.h
+++ sado_parameters.h
@@ -80,7 +80,7 @@ public:
   auto get_histbinx() const noexcept { return m_histbinx; }
 
   /// Number of histogram bins
-  int get_histw() const noexcept { return 200; }
+  int get_histw() const noexcept { return 50; }
 
   auto get_next_gen_method() const noexcept { return m_next_gen_method; }
 
