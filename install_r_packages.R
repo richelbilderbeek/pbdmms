beee8c3842a5c568d6fa965308dae273da6e9225
richelbilderbeek <richel@richelbilderbeek.nl>
2017-01-23 09:23:54 +0100

    Use newer DAISIE [skip ci]

--- install_r_packages.R
+++ install_r_packages.R
@@ -4,13 +4,13 @@ install.packages("devtools", repos = "http://cran.uk.r-project.org")
 # v1.2, does not support Endemic&NonEndemic yet
 install.packages("DAISIE", repos = "http://cran.uk.r-project.org")
 # v1.3, does support Endemic&NonEndemic, but is closed
-#devtools::install_github("richelbilderbeek/DAISIE_1_3", subdir = "DAISIE")
+devtools::install_github("richelbilderbeek/DAISIE_1_3", subdir = "DAISIE")
 
 devtools::install_github("richelbilderbeek/nLTT")
 devtools::install_github("rsetienne/PBD", subdir = "PBD")
 warnings()
 
-packageVersion("DAISIE")
+#packageVersion("DAISIE")
 packageVersion("devtools")
 packageVersion("nLTT")
 packageVersion("PBD")
