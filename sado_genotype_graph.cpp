d03c14c2ded2976a39281f4e719b20784d449c85
Lumphie <wesselinkkees@gmail.com>
2017-03-06 11:49:05 +0100

    Lets hope Heisenbug doesn't show up

--- sado_genotype_graph.cpp
+++ sado_genotype_graph.cpp
@@ -109,6 +109,10 @@ void sado::add_edges(
 
 bool sado::is_valid(const std::vector<std::vector<double>>& as) noexcept
 {
+  assert(!as.empty());
+  assert(is_square(as));
+  assert(has_diagonal_of_zeroes(as));
+
   return !as.empty()
     && is_square(as)
     && has_diagonal_of_zeroes(as) //Individuals do not find themselves attractive
