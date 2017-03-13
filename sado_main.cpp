84ff8bd7b870e183cc47173074d8e3ec154bf083
Lumphie <wesselinkkees@gmail.com>
2017-03-13 10:25:14 +0100

    Gets results in every runstate now

--- sado_main.cpp
+++ sado_main.cpp
@@ -36,6 +36,17 @@ int main(int argc, char *argv[])
   {
     simulation s(create_article_parameters());
     s.run();
+
+    const results res = s.get_results();
+    const std::vector<species> spp = res.get_species();
+    const auto g = create_graph_from_species_vector(spp);
+    const auto h = create_reconstructed(g);
+    save_graph_to_dot(h, "r_resultphylogeny.dot");
+    convert_dot_to_svg("r_resultphylogeny.dot", "r_resultphylogeny.svg");
+    convert_svg_to_png("r_resultphylogeny.svg", "r_resultphylogeny.png");
+    std::ofstream out("resultnewick");
+    out << to_newick(h);
+
     histogram_to_png("eco_traits.csv", "eco_traits.png");
     histogram_to_png("fem_prefs.csv", "fem_prefs.png");
     histogram_to_png("male_traits.csv", "male_traits.png");
@@ -45,6 +56,17 @@ int main(int argc, char *argv[])
   {
     simulation s(create_golden_standard_parameters());
     s.run();
+
+    const results res = s.get_results();
+    const std::vector<species> spp = res.get_species();
+    const auto g = create_graph_from_species_vector(spp);
+    const auto h = create_reconstructed(g);
+    save_graph_to_dot(h, "r_resultphylogeny.dot");
+    convert_dot_to_svg("r_resultphylogeny.dot", "r_resultphylogeny.svg");
+    convert_svg_to_png("r_resultphylogeny.svg", "r_resultphylogeny.png");
+    std::ofstream out("resultnewick");
+    out << to_newick(h);
+
     histogram_to_png("eco_traits.csv", "eco_traits.png");
     histogram_to_png("fem_prefs.csv", "fem_prefs.png");
     histogram_to_png("male_traits.csv", "male_traits.png");
@@ -54,11 +76,33 @@ int main(int argc, char *argv[])
   {
     simulation s(create_profiling_parameters());
     s.run();
+
+    const results res = s.get_results();
+    const std::vector<species> spp = res.get_species();
+    const auto g = create_graph_from_species_vector(spp);
+    const auto h = create_reconstructed(g);
+    save_graph_to_dot(h, "r_resultphylogeny.dot");
+    convert_dot_to_svg("r_resultphylogeny.dot", "r_resultphylogeny.svg");
+    convert_svg_to_png("r_resultphylogeny.svg", "r_resultphylogeny.png");
+    std::ofstream out("resultnewick");
+    out << to_newick(h);
+
     return 0;
   }
   const std::string filename{std::string(argv[1])};
   simulation s(read_parameters(filename));
   s.run();
+
+  const results res = s.get_results();
+  const std::vector<species> spp = res.get_species();
+  const auto g = create_graph_from_species_vector(spp);
+  const auto h = create_reconstructed(g);
+  save_graph_to_dot(h, "r_resultphylogeny.dot");
+  convert_dot_to_svg("r_resultphylogeny.dot", "r_resultphylogeny.svg");
+  convert_svg_to_png("r_resultphylogeny.svg", "r_resultphylogeny.png");
+  std::ofstream out("resultnewick");
+  out << to_newick(h);
+
   histogram_to_png("eco_traits.csv", "eco_traits.png");
   histogram_to_png("fem_prefs.csv", "fem_prefs.png");
   histogram_to_png("male_traits.csv", "male_traits.png");
