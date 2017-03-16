61294e74e3a10ea61cf8af9d426750e8db18d7e2
richelbilderbeek <richel@richelbilderbeek.nl>
2017-03-16 08:49:32 +0100

    Install DAISIE from CRAN

--- install_r_packages.R
+++ install_r_packages.R
@@ -1,8 +1,10 @@
 # Installs the R packages needed
-install.packages("devtools", repos = "http://cran.uk.r-project.org")
+my_repo <- "http://cran.us.r-project.org"
+
+install.packages("devtools", repos = my_repo)
 
 # v1.2, does not support Endemic&NonEndemic yet
-install.packages("DAISIE", repos = "http://cran.uk.r-project.org")
+install.packages("DAISIE", repos = my_repo)
 # v1.3, does support Endemic&NonEndemic, but is closed
 # devtools::install_github("richelbilderbeek/DAISIE_1_3", subdir = "DAISIE")
 
