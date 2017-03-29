a198441f3ddd24cd2d6c4ad452853524c4bf5e56
richelbilderbeek <richel@richelbilderbeek.nl>
2017-03-29 16:48:17 +0200

    EOD

--- newick_to_png_main.cpp
+++ newick_to_png_main.cpp
@@ -16,12 +16,12 @@ int main(int argc, char *argv[])
       << '\n'
       << "Use:\n"
       << '\n'
-      << "./newick_to_png [newick]\n"
+      << "./newick_to_png \"[newick]\"\n"
       << '\n'
       << "The Newick will be saved to newick.png\n"
       << "For example:"
       << '\n'
-      << "./newick_to_png (:1,:1):1;\n"
+      << "./newick_to_png \"(:1,:1):1;\"\n"
     ;
   }
   newick_to_picture(argv[1], "newick.png");
